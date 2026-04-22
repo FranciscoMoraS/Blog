e8fe501b4bf69d8cb875c50f199506f837e13a32
FranciscoMoraS <franmora1205@gmail.com>
2026-04-22 15:42:54 -0600

    Add files via upload

---
+++ Ejercicio del blog/src/logica/comentario.java
@@ -0,0 +1,21 @@
+package logica;
+
+import java.time.LocalDateTime;
+
+public class comentario {
+	private LocalDateTime fechaCreacion;
+	private String email;
+	private String ip;
+	private String texto;
+	
+	public comentario(String email, String ip, String texto) {
+		this.email=email;
+		this.ip=ip;
+		this.texto=texto;
+		fechaCreacion= LocalDateTime.now();
+	}
+	public String toString() {
+		
+	}
+	
+}
